47d48ad2d0690e23ade8b3595b7fd1cd90c97a8b
1chary <abhilashchary04@gmail.com.>
2024-07-16 16:30:24 +0530

    created sign up page with functionality static login page

--- src/components/LoginComponent/index.js
+++ src/components/LoginComponent/index.js
@@ -1,6 +1,26 @@
+import { useState } from "react"
+import "./index.css"
 
-const LoginComponent = () => (
-    <h1>Login page</h1>
-)
+const LoginComponent = () => {
+    const [username,setLogInUsername] = useState("")
+    const [password, setLogInPassword] = useState("")
+    return (
+    <div className="sign-up-page">
+        <div className="image-container-for-login"></div>
+        <div className = "details-container">
+                <form className = "form-container">
+                    <h1 className = "register-heading">Log In</h1>
+                    <div className = "alignment-container">
+                        <label className = "label-styling" htmlFor = "username" >Username</label>
+                        <input type = "text" className = "input-field-box" id = "username" placeholder = "Enter your Name" onChange = {(event) => setLogInUsername(event.target.value)}/>
+                        <label className = "label-styling" htmlFor = "password">Password</label>
+                        <input type = "password" className = "input-field-box" id = "password" placeholder = "Enter your password" onChange = {(event) => setLogInPassword(event.target.value)}/>
+                        <button type = "submit" className="log-in-button">Log In</button>
+                    </div>
+                </form>
+            </div>
+    </div>
+    )
+}
 
 export default LoginComponent
--- src/components/SignUpComponent/index.js
+++ src/components/SignUpComponent/index.js
@@ -1,19 +1,51 @@
-import "./index.css"
+import "./index.css" 
+import {useState} from "react"
+import {Link} from "react-router-dom"
 
+const SignUpComponent = () => {
+    const [username,setUsername] = useState("")
+    const [email,setEmail] = useState("")
+    const [pass,setPassword] = useState("")
+    const [number,setNumber] = useState("")
 
-const SignUpComponent = () => (
-    <div className="sign-up-page">
-        <div className="image-container">
-            <h1 className="welcome-heading">Welcome to the Sign In page</h1>
-            <p className="connection-heading">Connect to the millions around the world by signing in</p>
+    const submitTheDetails = async (event) => {
+        event.preventDefault()
+        /*static data said to include in the doc */
+        const lastName = "ni";
+        const userCity = "Hyderabad";
+        const zipCode = "500072";
+        const userDetails = {username,email,pass,number,lastName,userCity,zipCode}
+        const options = {
+            body: JSON.stringify(userDetails),
+            method: 'POST',
+        }
+        const apiUrl = "https://syoft.dev/Api/user_registeration/api/user_registeration"
+        const responseData = await fetch(apiUrl,options)
+    }
+
+    return (
+        <div className="sign-up-page">
+            <div className="image-container"></div>
+            <div className = "details-container">
+                <form className = "form-container" onSubmit={submitTheDetails}>
+                    <h1 className = "register-heading">Register</h1>
+                    <div className = "alignment-container">
+                        <label className = "label-styling" htmlFor = "username" >Username</label>
+                        <input type = "text" className = "input-field-box" id = "username" placeholder = "Enter your Name" onChange = {(event) => setUsername(event.target.value)}/>
+                        <label className = "label-styling" htmlFor = "email">E mail</label>
+                        <input type = "text" className = "input-field-box" id = "email" placeholder = "Enter your e mail address" onChange = {(event) => setEmail(event.target.value)}/>
+                        <label className = "label-styling" htmlFor = "password">Password</label>
+                        <input type = "password" className = "input-field-box" id = "password" placeholder = "Enter password" onChange = {(event) => setPassword(event.target.value)}/>
+                        <label className = "label-styling" htmlFor = "number">Mobile Number</label>
+                        <input type = "text" className = "input-field-box" id = "number" placeholder = "Enter your phone number" onChange = {(event) => setNumber(event.target.value)}/>
+                        <button type = "submit" className="sign-in-button">Sign In</button>
+                        <p className="have-an-account-para">Already have an account?<Link to = "/logIn" className="log-in-text"> Log In</Link></p>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div className = "details-container">
-            <form className = "form-container">
-                <h1 className = "register-heading">Register</h1>
-            </form>
-        </div>
-    </div>
+    )
     
-)
+}
 
 export default SignUpComponent
